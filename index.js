8ff5e68e2ae7676cffcf6b523cb198f12d567163
vilorshini ramsamy <vilorshiniramsamy02@gmail.com>
2021-12-03 20:19:57 +0400

    added background animation

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 
 let burger = document.querySelector(".burger");
 burger.addEventListener("click", () => burger.classList.toggle("activebrgr"));
+
+//animated text//
+
+let textType = document.querySelector('.typeing')
+let textArray = textType.dataset.typeingtext.split("")
+let typeCount = 0;
+
+let autoTypeText = () => {
+    if(typeCount < textType.dataset.typeingtext.length){
+        textType.innerHTML += textType.dataset.typeingtext.charAt(typeCount)
+        typeCount++
+        textArray = textType.dataset.typeingtext.split("")
+    }else {
+        textArray.pop()
+        textType.innerHTML = textArray.join("")
+        if(textArray.length == 0){
+            typeCount = 0
+        }
+    }  
+}
+setInterval(() => {
+    autoTypeText()
+},175)
+
+
+
